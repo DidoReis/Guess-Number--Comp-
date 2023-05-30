683c5913e56c7c73c2ea0f3d8cc8a393902016aa
DidoReis <101334003+DidoReis@users.noreply.github.com>
2023-05-30 12:27:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
 import random
 
 def guess_number():
-    lower_limit = 1
-    upper_limit = 100
-    attempts = 0
+    lower_limit = 1     # Set the lower limit of the number range
+    upper_limit = 100   # Set the upper limit of the number range
+    attempts = 0        # Initialize the number of attempts
 
     print("Think of a number between 1 and 100 and type 'ok' when you're ready.")
-    response = input()
+    response = input()  # Prompt the user to start the game
 
     if response.lower() != 'ok':
         print("Sorry, I didn't understand your response. Please type 'ok' to begin.")
-        return
+        return   # Exit the function if the response is not 'ok'
 
     while True:
-        guess = random.randint(lower_limit, upper_limit)
-        print("I'm guessing the number:", guess)
+        guess = random.randint(lower_limit, upper_limit)  # Generate a random guess within the limits
+        print("I'm guessing the number:", guess)  # Display the current guess
         response = input("Is the number higher (>), lower (<), or equal (=) to the guess? ")
 
         if response == '=':
-            print("Correct! The number is", guess)
+            print("Correct! The number is", guess)  # The guess is correct, display the message and break the loop
             break
         elif response == '>':
-            lower_limit = guess + 1
+            lower_limit = guess + 1  # Adjust the lower limit based on the guess
         elif response == '<':
-            upper_limit = guess - 1
+            upper_limit = guess - 1  # Adjust the upper limit based on the guess
         else:
             print("Sorry, I didn't understand your response. Please use '>', '<', or '='.")
 
-        attempts += 1
+        attempts += 1  # Increment the number of attempts
 
-    print("Number of attempts:", attempts)
+    print("Number of attempts:", attempts)  # Display the total number of attempts
 
-guess_number()
+guess_number()  # Call the function to start the game
